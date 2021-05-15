7e1b726825fdaf45c842ed19e01f4d4d25233487
Maximilien Andile <maximilien@practical-go-lessons.com>
2021-05-15 09:42:42 +0000

    Add Thumbnail

--- src/App.vue
+++ src/App.vue
@@ -6,29 +6,56 @@
         <Carousel></Carousel>
       </div>
     </div>
-
-    <div class="row text-start">
-      <div class="col-6 col-md-2 border">
-        Text 1
+    <h3 class="mt-3">Popular products</h3>
+    <div class="row">
+      <div class="col-12 col-md-3 d-flex justify-content-center mt-3">
+        <ProductThumbnail
+            price="10 EUR"
+            title="Product Name"
+            additional-info="New"
+        ></ProductThumbnail>
       </div>
-      <div class="col-6 col-md-8 border">
-        Text 2
+      <div class="col-12 col-md-3 d-flex justify-content-center mt-3">
+        <ProductThumbnail
+            price="15 EUR"
+            title="Product Name 2"
+            additional-info="Sold Out"
+        ></ProductThumbnail>
       </div>
-      <div class="col col-md-2 border">
-        Text 3
+      <div class="col-12 col-md-3 d-flex justify-content-center mt-3">
+        <ProductThumbnail
+            price="15 EUR"
+            title="Product Name 2"
+            additionalInfo="Sold Out"
+        ></ProductThumbnail>
+      </div>
+      <div class="col-12 col-md-3 d-flex justify-content-center mt-3">
+        <ProductThumbnail
+            price="15 EUR"
+            title="Product Name 2"
+            additional-info="Sold Out"
+        ></ProductThumbnail>
+      </div>
+      <div class="col-12 col-md-3 d-flex justify-content-center mt-3">
+        <ProductThumbnail
+            price="15 EUR"
+            title="Product Name 2"
+            additional-info="Sold Out"
+        ></ProductThumbnail>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
 import NavBar from "./components/NavBar";
 import Carousel from "./components/Carousel";
+import ProductThumbnail from "./components/ProductThumbnail";
 
 export default {
   name: 'App',
   components: {
+    ProductThumbnail,
     Carousel,
     NavBar
   }
