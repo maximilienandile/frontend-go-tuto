733736e3e5db723c8f89b22899fd6f52f6d9f4c5
Maximilien Andile <maximilien@practical-go-lessons.com>
2021-05-15 09:42:42 +0000

    add loader and product info

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -1,29 +1,46 @@
 <template>
   <NavBar></NavBar>
-  <p>{{productId}}</p>
-  <p>{{product}}</p>
+  <Loader v-if="loading"></Loader>
+  <div v-else class="row">
+    <div class="col">
+      <img :src="product.image" :alt="product.name">
+    </div>
+    <div class="col">
+      <h1>{{product.name}}</h1>
+      <p>{{product.totalPrice.display}}</p>
+      <p>{{product.shortDescription}}</p>
+    </div>
+
+  </div>
+
+
 
 </template>
 
 <script>
 import NavBar from "../components/NavBar";
 import Webservice from "../webservice";
+import Loader from "../components/Loader";
 export default {
   name: "ProductDetail",
-  components: {NavBar},
+  components: {Loader, NavBar},
   props: {
     productId: String
   },
   data(){
     return {
       product: null,
+      loading: false,
     }
   },
   created() {
+    this.loading = true
     Webservice.getProductByID(this.productId).then((res)=> {
       this.product = res.data
+      this.loading = false
     }).catch((err)=>{
       alert(err)
+      this.loading = false
     })
   }
 }
