c58e764df588a1c9bdf47c8701e2d08441e8c8b5
Maximilien Andile <maximilien@practical-go-lessons.com>
2021-05-15 09:42:42 +0000

    product detail end

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -1,16 +1,30 @@
 <template>
   <NavBar></NavBar>
   <p>{{productId}}</p>
+  <p>{{product}}</p>
 
 </template>
 
 <script>
 import NavBar from "../components/NavBar";
+import Webservice from "../webservice";
 export default {
   name: "ProductDetail",
   components: {NavBar},
   props: {
     productId: String
+  },
+  data(){
+    return {
+      product: null,
+    }
+  },
+  created() {
+    Webservice.getProductByID(this.productId).then((res)=> {
+      this.product = res.data
+    }).catch((err)=>{
+      alert(err)
+    })
   }
 }
 </script>
--- src/webservice/index.js
+++ src/webservice/index.js
@@ -9,7 +9,12 @@ function getCategories(){
     return axios.get(process.env.VUE_APP_API_BASE_URL+"/categories")
 }
 
+function getProductByID(productId){
+    return axios.get(process.env.VUE_APP_API_BASE_URL+"/product/"+productId)
+}
+
 export default {
     getProducts:getProducts,
-    getCategories:getCategories
+    getCategories:getCategories,
+    getProductByID:getProductByID
 }
